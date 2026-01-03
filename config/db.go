6527798b766dcaa3856ac4e5892a343cb08f77f8
HeroBuxx <me@herobuxx.me>
2026-01-03 19:30:40 +0700

    godhelpme

--- config/db.go
+++ config/db.go
@@ -11,15 +11,15 @@ import (
 var DB *sql.DB
 
 func ConnectDB() {
-	// Register a custom TLS config that skips cert verification (DEV ONLY).
-	err := mysql.RegisterTLSConfig("skip-verify", &tls.Config{
+	// Register custom TLS config (DEV ONLY)
+	err := mysql.RegisterTLSConfig("dev", &tls.Config{
 		InsecureSkipVerify: true,
 	})
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	dsn := "mariadb:7F37RDMdizgkWC75q5myFH6QFGFzwFK7JTmkR5mQMK9yYkSxzAShdkDZUf2k0GzP@tcp(fs48ws4oo88o4gsk4os4wwos:3306)/default?parseTime=true&tls=skip-verify"
+	dsn := "mariadb:7F37RDMdizgkWC75q5myFH6QFGFzwFK7JTmkR5mQMK9yYkSxzAShdkDZUf2k0GzP@tcp(fs48ws4oo88o4gsk4os4wwos:3306)/default?parseTime=true&tls=dev"
 
 	DB, err = sql.Open("mysql", dsn)
 	if err != nil {
