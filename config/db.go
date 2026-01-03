9e05943a3786a59e23f7badb20e27747395747a7
HeroBuxx <me@herobuxx.me>
2026-01-03 19:24:56 +0700

    bruh2

--- config/db.go
+++ config/db.go
@@ -1,18 +1,25 @@
 package config
 
 import (
+	"crypto/tls"
 	"database/sql"
 	"log"
 
-	_ "github.com/go-sql-driver/mysql"
+	"github.com/go-sql-driver/mysql"
 )
 
 var DB *sql.DB
 
 func ConnectDB() {
-	var err error
+	// Register a custom TLS config that skips cert verification (DEV ONLY).
+	err := mysql.RegisterTLSConfig("skip-verify", &tls.Config{
+		InsecureSkipVerify: true,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	dsn := "mariadb:7F37RDMdizgkWC75q5myFH6QFGFzwFK7JTmkR5mQMK9yYkSxzAShdkDZUf2k0GzP@tcp(fs48ws4oo88o4gsk4os4wwos:3306)/default?parseTime=true&tls=true"
+	dsn := "mariadb:7F37RDMdizgkWC75q5myFH6QFGFzwFK7JTmkR5mQMK9yYkSxzAShdkDZUf2k0GzP@tcp(fs48ws4oo88o4gsk4os4wwos:3306)/default?parseTime=true&tls=skip-verify"
 
 	DB, err = sql.Open("mysql", dsn)
 	if err != nil {
