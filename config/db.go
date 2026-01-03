79623974c41fbc5342ee2ea340b61b929944f672
HeroBuxx <me@herobuxx.me>
2026-01-03 19:12:34 +0700

    uriiiiiiiiiiiiii

--- config/db.go
+++ config/db.go
@@ -12,7 +12,8 @@ var DB *sql.DB
 func ConnectDB() {
 	var err error
 
-	dsn := "mariadb:7F37RDMdizgkWC75q5myFH6QFGFzwFK7JTmkR5mQMK9yYkSxzAShdkDZUf2k0GzP@tcp(87.239.129.130:3306)/default?parseTime=true&tls=skip-verify"
+	dsn := "mariadb:7F37RDMdizgkWC75q5myFH6QFGFzwFK7JTmkR5mQMK9yYkSxzAShdkDZUf2k0GzP@tcp(fs48ws4oo88o4gsk4os4wwos:3306)/default?parseTime=true"
+
 	DB, err = sql.Open("mysql", dsn)
 	if err != nil {
 		log.Fatal(err)
